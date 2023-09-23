18e6ad4585fac9f68c011b1913038ef459a66e27
egrmadnik <edvard.grmadnik@gmail.com>
2023-09-23 23:47:22 +0200

    Initial commit

---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+  <q-page class="q-pl-lg">
+    <p class="text-h6 q-pt-md">O programu</p>
+    <NuxtLink to="/">Home</NuxtLink>
+  </q-page>
+</template>
---
+++ pages/team.vue
@@ -0,0 +1,6 @@
+<template>
+  <q-page class="q-pl-lg">
+    <p class="text-h6 q-pt-md">Ekipa</p>
+    <NuxtLink to="/">Home</NuxtLink>
+  </q-page>
+</template>
